763c7b187caf8acbec562d27f2406ac3bb1c1170
Dennis Silva <1156559+dasdennis@users.noreply.github.com>
2025-05-29 17:40:11 -0300

    refact: Update Overpass API service

--- app/services/overpass_service.rb
+++ app/services/overpass_service.rb
@@ -1,15 +1,24 @@
 class OverpassService
   def self.fetch_trails(bbox)
     Rails.cache.fetch("trails#{bbox}", expires_in: 12.hours) do
-      overpass = OverpassAPI.new
-      query = "[out:json];way[highway=path][foot=designated](#{bbox});out geom;"
-      response = overpass.query(query)
-      process_response(response)
+      overpass = OverpassAPI.new(timeout = 30)
+      query = <<~QUERY
+        [out:json][timeout:30];
+        way[highway=path][foot=designated](#{bbox});
+        out body geom;
+      QUERY
+
+      process_response(overpass.query(query))
     end
+  rescure => e
+    Rails.logger.error "Overpass error: #{e.message}"
   end
 
   private
 
+  # Processes the Overpass API response to extract trail data
+  # and create Trail records.
+  # Continue from here
   def self.process_response(response)
     response[:elements].filter_map do |element|
       next unless element[:geometry]
