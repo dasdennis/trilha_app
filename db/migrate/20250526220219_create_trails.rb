0cec45629525280cda97c89068beb2de73fc9dcf
Dennis Silva <1156559+dasdennis@users.noreply.github.com>
2025-05-28 22:00:20 -0300

    refact: Change create_trails migration

--- db/migrate/20250526220219_create_trails.rb
+++ db/migrate/20250526220219_create_trails.rb
@@ -1,10 +1,17 @@
 class CreateTrails < ActiveRecord::Migration[7.2]
   def change
     create_table :trails do |t|
-      t.string :name
-      t.string :difficulty
-
+      t.string :name, null: false
+      t.string :difficulty, null: false
+      t.line_string :path, geographic: true, srid: 4326
+      t.bigint :osm_id, null: false, index: { unique: true }
       t.timestamps
     end
+
+    add_check_constraint :trails, "difficulty IN ('easy', 'moderate', 'difficult', 'unknown')", name: "difficulty_check"
+    add_index :trails, :name
+    add_index :trails, :difficulty
   end
 end
+# SRID 4326 = WGS84 standard for GPS
+# OSM ID uniqueness prevents duplicate imports
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_19_160224) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_26_220219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
 
+  create_table "trails", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "difficulty", null: false
+    t.geography "path", limit: {:srid=>4326, :type=>"line_string", :geographic=>true}
+    t.bigint "osm_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["difficulty"], name: "index_trails_on_difficulty"
+    t.index ["name"], name: "index_trails_on_name"
+    t.index ["osm_id"], name: "index_trails_on_osm_id", unique: true
+    t.check_constraint "difficulty::text = ANY (ARRAY['easy'::character varying, 'moderate'::character varying, 'difficult'::character varying, 'unknown'::character varying]::text[])", name: "difficulty_check"
+  end
 end
